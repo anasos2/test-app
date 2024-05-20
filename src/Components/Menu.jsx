0a9e229d7989b529a71dabc7ee72ce87edce8fec
Anasos2 <anasgvvc@gmail.com>
2024-05-20 11:48:07 +0100

    Update Menu and localization files

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -43,12 +43,20 @@ function Menu({ closeMenu }) {
         </Link>
       </li>
       <li>
-        <a
-          href="/perday"
+        <Link
+         to="/perday"
           className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
         >
           {t('menu.consumption')}
-        </a>
+        </Link>
+      </li>
+      <li>
+        <Link
+         to="/language"
+          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+        >
+          {t('menu.language')}
+        </Link>
       </li>
 
      
